801e8878c9acc470f6421694d9651c7f961bdfd4
tryzediv <tmn.harin.d@gmail.com>
2023-02-21 14:24:45 +0400

    Create skillfactory_17_7_3.py

---
+++ skillfactory_17_7_3.py
@@ -0,0 +1,19 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+money = input('Введите сумму, которую Вы планируете положить под проценты: ')
+
+#Решение с циклом for
+if money.isdigit():
+    for profit in per_cent.values():
+        deposit.append(int((int(money) * profit) / 100))
+    print('Максимальная сумма, которую вы можете заработать — {}'.format(max(deposit)))
+else:
+    print('Пожалуйста введите число')
+
+
+#Решение без циклов
+deposit.append(per_cent['ТКБ'] * int(money) / 100)
+deposit.append(per_cent['СКБ'] * int(money) / 100)
+deposit.append(per_cent['ВТБ'] * int(money) / 100)
+deposit.append(per_cent['СБЕР'] * int(money) / 100)
+print('Максимальная сумма, которую вы можете заработать — {}'.format(max(deposit)))
