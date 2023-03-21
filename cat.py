b3c0ea5f1fe0f9ed1c63b0733d5b603930f99de3
tryzediv <tmn.harin.d@gmail.com>
2023-03-21 18:07:10 +0400

    Add dogs class

--- cat.py
+++ cat.py
@@ -1,14 +1,33 @@
-class Cats:
-    def __init__(self, gender, name, age):
-        self.gender = gender
+class Animals:
+    def __init__(self, type_animal, name):
+        self.type_animal = type_animal
+        self.name = name
+
+    def info_name(self):
+        print(f'Я {self.type_animal}, по имени {self.name}')
+
+
+class Cats(Animals):
+    def __init__(self, name, age):
+        super().__init__('кошка', name)
         self.name = name
         self.age = age
 
-    def get_gender(self):
-        return self.gender
+    def info(self):
+        print(f'Я кошка по имени {self.name}, мне {self.age} лет')
 
-    def get_name(self):
-        return self.name
+    def sound(self):
+        print('Мяу')
 
-    def get_age(self):
-        return self.age
+
+class Dogs(Animals):
+    def __init__(self, name, age):
+        super().__init__('собака', name)
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f'Я собака по имени {self.name}, мне {self.age} лет')
+
+    def sound(self):
+        print('Гав')
