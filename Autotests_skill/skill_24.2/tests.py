80fbe8cf3e4075528479fa1e353c0517f79f5f63
tryzediv <tmn.harin.d@gmail.com>
2023-04-17 21:45:42 +0500

    Добавлены тесты калькулятора

--- Autotests_skill/skill_24.2/tests.py
+++ Autotests_skill/skill_24.2/tests.py
@@ -1,9 +1,5 @@
 import pytest
-from calculator import Calculator
-import random
-
-x = random.randint(0, 255)
-y = random.randint(0, 255)
+from app.calculator import Calculator
 
 
 class TestsCalc:
@@ -11,19 +7,23 @@ class TestsCalc:
         self.calculator = Calculator
 
     def test_multyply_success(self):
-        assert self.calculator.multiply(self, x, y) == x * y
+        assert self.calculator.multiply(self, 5, 6) == 30, ''
 
     def test_adding_success(self):
-        assert self.calculator.adding(self, x, y) == x + y
+        assert self.calculator.adding(self, 12, 21) == 33, ''
+
+    def test_division_success(self):
+        assert self.calculator.division(self, 12, 4) == 3, ''
+
+    def test_subtraction_success(self):
+        assert self.calculator.subtraction(self, 24, 4) == 20, ''
 
     def test_adding_unsuccess(self):
-        assert self.calculator.adding(self, 2, 2) != 5
+        assert self.calculator.adding(self, 2, 2) != 5, ''
 
     def test_zero_division(self):
-        if y == 0:
-            with pytest.raises(ZeroDivisionError) as e:
-                assert self.calculator.division(self, x, y)
-        else:
-            assert self.calculator.division(self, x, y) == x / y
+        with pytest.raises(ZeroDivisionError) as e:
+            assert self.calculator.division(self, 12, 0), ''
+
     def teardown(self):
         print('End of test')
